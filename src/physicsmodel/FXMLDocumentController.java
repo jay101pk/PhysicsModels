f5f71cc2148d866e7dea3b2304473ec0d0e4030a
logerquist3873 <logerquist3873@C-15PCD056.d211.org>
2016-04-15 12:33:09 -0500

    initialization

--- src/physicsmodel/FXMLDocumentController.java
+++ src/physicsmodel/FXMLDocumentController.java
@@ -5,12 +5,17 @@
  */
 package physicsmodel;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -21,10 +26,25 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Label label;
     
+    private Stage stage=new Stage();
+    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleButtonAction(ActionEvent event){
+        try{
+            Parent root2 = FXMLLoader.load(getClass().getResource("FXMLDocument1.fxml"));
+        
+        Scene scene = new Scene(root2);
+        stage.setScene(scene);
+        stage.show();
+        }
+        catch(IOException error){
+            System.out.println("didnt work");
+        }
+    }
+    
+    @FXML
+    private void handleClose(ActionEvent event){
+        stage.close();
     }
     
     @Override
